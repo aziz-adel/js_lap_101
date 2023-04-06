6a8604e82bfd28ede3e0a48e10d9c52cd67c115b
aziz-adel <abdulaziz.adel@gmail.com>
2023-04-06 22:59:02 +0300

    nodejs

--- main.js
+++ main.js
@@ -4,3 +4,69 @@ console.log(coclr);
 
 color="black";
 console.log(color)
+// next ex (type of data):
+let name ="aziz"
+let age =23
+ console.log(typeof name)
+ console.log(typeof age)
+
+ // next ex (boolean):
+ let result = 2>3;
+ console.log(result);
+  result = 3>2;
+ console.log(result);
+ console.log(typeof result);
+
+ // next ex (string):
+ let massge ='welcome to js ';
+ console.log(typeof massge);
+ massge ='1234';
+ console.log(typeof massge);
+ massge =1234;
+ console.log(typeof massge);
+
+  // next ex (Template Literals):
+let lang ='C++'
+let msg =`welcome to ${lang}`
+console.log(msg);
+// next ex (Undefined):
+let n ;
+console.log (typeof n)
+// next ex (Number):
+let value = 5;
+console.log(typeof value);
+
+ value = -5;
+console.log(typeof value);
+value = 2.5;
+console.log(typeof value);
+// next ex (Nan):
+console.log(value * 'js')
+
+// quiz:
+let valOne = 100 >= 20
+let valTwo = 10 >= 20
+console.log(valOne == valTwo)
+
+let phoneNumber = "0555";
+console.log(typeof phoneNumber)
+
+// next ex (String Length):
+let tex = "roianhfgioanr"
+console.log(tex.length)
+// next ex (String Concatenation):
+let a = 'welcome to ';
+let b= 'my website';
+console.log(a+b);
+a = 'welcome to ';
+ b= 'my website'+a;
+ console.log(a+b);
+ // next ex (Bracket Notation):
+ let bre='javascript';
+ console.log (bre[4]);
+ //quiz
+ let greeting = "Hello"
+ name = "Mariam";
+let message = `${greeting}
+${name}!`;
+console.log(message);
